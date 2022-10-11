c071513be67d07b9798984c3e678135effa0dd2e
Woonki Moon <woonki.moon@gmail.com>
2022-10-11 23:16:20 +0900

    use create_await_expression fn

--- jscomp/frontend/ast_await.ml
+++ jscomp/frontend/ast_await.ml
@@ -12,66 +12,46 @@ let create_await_module_expression (e : Parsetree.module_expr) =
   in
   let module_expr_without_attr = { e with pmod_attributes = [] } in
   let import_pexp_desc = Parsetree.Pexp_ident { txt; loc = e.pmod_loc } in
-  let txt =
-    Longident.Ldot (Longident.Ldot (Lident "Js", "Promise"), "unsafe_await")
-  in
-  let await_pexp_desc = Parsetree.Pexp_ident { txt; loc = e.pmod_loc } in
   {
     module_expr_without_attr with
     pmod_desc =
       Pmod_unpack
-        {
-          pexp_desc =
-            Pexp_apply
-              ( {
-                  pexp_desc = await_pexp_desc;
-                  pexp_attributes = [];
-                  pexp_loc = e.pmod_loc;
-                },
-                [
-                  ( Nolabel,
-                    {
-                      pexp_desc =
-                        Pexp_apply
-                          ( {
-                              pexp_desc = import_pexp_desc;
-                              pexp_attributes = [];
-                              pexp_loc = e.pmod_loc;
-                            },
-                            [
-                              ( Nolabel,
-                                {
-                                  pexp_desc =
-                                    Pexp_constraint
-                                      ( {
-                                          pexp_desc =
-                                            Pexp_pack module_expr_without_attr;
-                                          pexp_loc = e.pmod_loc;
-                                          pexp_attributes = [];
-                                        },
-                                        {
-                                          ptyp_desc =
-                                            Ptyp_package
-                                              ( {
-                                                  txt = Lident "BeltList";
-                                                  loc = e.pmod_loc;
-                                                },
-                                                [] );
-                                          ptyp_loc = e.pmod_loc;
-                                          ptyp_attributes = [];
-                                        } );
-                                  pexp_attributes = [];
-                                  pexp_loc = e.pmod_loc;
-                                } );
-                            ] );
-                      pexp_attributes =
-                        [
-                          ({ txt = "res.await"; loc = Location.none }, PStr []);
-                        ];
-                      pexp_loc = e.pmod_loc;
-                    } );
-                ] );
-          pexp_loc = e.pmod_loc;
-          pexp_attributes = [];
-        };
+        (create_await_expression
+           {
+             pexp_desc =
+               Pexp_apply
+                 ( {
+                     pexp_desc = import_pexp_desc;
+                     pexp_attributes = [];
+                     pexp_loc = e.pmod_loc;
+                   },
+                   [
+                     ( Nolabel,
+                       {
+                         pexp_desc =
+                           Pexp_constraint
+                             ( {
+                                 pexp_desc = Pexp_pack module_expr_without_attr;
+                                 pexp_loc = e.pmod_loc;
+                                 pexp_attributes = [];
+                               },
+                               {
+                                 ptyp_desc =
+                                   Ptyp_package
+                                     ( {
+                                         txt = Lident "BeltList";
+                                         loc = e.pmod_loc;
+                                       },
+                                       [] );
+                                 ptyp_loc = e.pmod_loc;
+                                 ptyp_attributes = [];
+                               } );
+                         pexp_attributes = [];
+                         pexp_loc = e.pmod_loc;
+                       } );
+                   ] );
+             pexp_attributes =
+               [ ({ txt = "res.await"; loc = Location.none }, PStr []) ];
+             pexp_loc = e.pmod_loc;
+           });
   }
